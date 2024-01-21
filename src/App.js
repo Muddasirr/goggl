45b8af329861134963f3cc5420ec3276444de8ee
Muddasirr <110295334+Muddasirr@users.noreply.github.com>
2024-01-21 18:08:44 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import Navbar from './components/Navbar';
 import Routes from './components/Routes';
 import Footer from './components/Footer';
 import {react,useState} from 'react';
+import { Router } from 'react-router-dom';
 
 function App() {
   const[darkTheme, setDarkTheme] = useState(false)
   return (
+  
     <div className={darkTheme?'dark':''}  >
       <div className="bg-gray-100 dark:bg-gray-900 
       dark:text-gray-200 min-h-screen">
-        <Navbar/>
+        <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
         <Routes/>
         <Footer/>
       </div>
      
     </div>
+  
   );
 }
 
